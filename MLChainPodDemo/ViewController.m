7f486f8ea8f96a6d96e7940080f9478739d8c54e
luomagaoshou <miaolonglai@miaolonglaideMacBook-Pro.local>
2017-05-23 13:55:57 +0800

    fix 多个类生成时顺序未按类继承顺序排列

--- MLChainPodDemo/ViewController.m
+++ MLChainPodDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import <MLChain/MLChain.h>
 @interface ViewController ()
-
+@property (nonatomic, assign) NSInteger a;
 @end
 
 @implementation ViewController
@@ -21,9 +21,79 @@
     
     UIView *view = UIView.mlc_make.frame_(100, 100, 100, 100).backgroundColor([UIColor cyanColor]).chainObject;
     [self.view addSubview:view];
+  
+
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self addInGolbal];
+        [self addInMain];
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            [self addInGolbal];
+            [self addInMain];
+            
+        });
+    });
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self addInGolbal];
+        [self addInMain];
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            [self addInGolbal];
+            [self addInMain];
+        });
+    });
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self addInGolbal];
+        [self addInMain];
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            [self addInGolbal];
+            [self addInMain];
+        });
+    });
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self addInGolbal];
+        [self addInMain];
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            [self addInGolbal];
+            [self addInMain];
+        });
+    });
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        [self addInGolbal];
+        [self addInMain];
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            [self addInGolbal];
+            [self addInMain];
+        });
+    });
+
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSLog(@"%ld", self.a);
+    });
     
 }
 
+- (void)addInGolbal {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        for (NSInteger i = 0; i < 1000; i++) {
+            self.a = self.a + 1;
+        }
+       // NSLog(@"%@-%ld", NSStringFromSelector(_cmd), self.a);
+    });
+    
+}
+- (void)addInMain {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        for (NSInteger i = 0; i < 1000; i++) {
+            self.a = self.a + 1;
+        }
+         //NSLog(@"%@-%ld", NSStringFromSelector(_cmd), self.a);
+    });
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
